3f3c3dd0534916f1b427fd7c16a317c1eab3c9ef
rainesjared <raines.j@northeastern.edu>
2021-09-28 11:16:19 -0400

    added branch for java driver of VAEM

---
+++ examples/java/example.java
@@ -0,0 +1,24 @@
+import driver.VaemDriver;
+
+import static java.lang.Thread.sleep;
+
+public class example {
+    public static void main(String[] args) throws InterruptedException {
+        int[] valveData = {500, 0, 0, 0, 0, 0, 0, 0};
+        VaemDriver vm = new VaemDriver("192.168.0.214", 502);
+        vm.init();
+        vm.configureValves(valveData);
+
+        while (true) {
+            sleep(1000);
+            vm.openValve();
+            sleep(1000);
+            vm.readStatus();
+            vm.closeValve();
+        }
+
+
+
+    }
+
+}
