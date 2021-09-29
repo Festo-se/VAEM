6f260cc18b12585644d7307b591503126a46f7e7
rainesjared <71296226+rainesjared@users.noreply.github.com>
2021-09-29 09:23:50 -0400

    Update and rename example.java to exampleVaem.java
    Renamed example file for continuity.

--- examples/java/exampleVaem.java
+++ examples/java/exampleVaem.java
@@ -2,7 +2,7 @@ import driver.VaemDriver;
 
 import static java.lang.Thread.sleep;
 
-public class example {
+public class exampleVaem {
     public static void main(String[] args) throws InterruptedException {
         int[] valveData = {500, 0, 0, 0, 0, 0, 0, 0};
         VaemDriver vm = new VaemDriver("192.168.0.214", 502);
