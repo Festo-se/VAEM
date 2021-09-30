17c1525361119939dfac839da84bf841887a30d8
rainesjared <raines.j@northeastern.edu>
2021-09-30 16:02:33 -0400

    rewrite example script

--- examples/c#/src/example.cs
+++ examples/c#/src/example.cs
@@ -6,8 +6,12 @@ class Example {
                 VaemDriver driver = new VaemDriver("192.168.0.207", 502);
                 driver.ConfigureVaem();
                 driver.SaveSettings();
-                driver.OpenValve();
-                driver.CloseValve();
-                driver.ReadStatus();
+                while(1) {
+                    sleep(1000);
+                    driver.OpenValve();
+                    sleep(1000);
+                    driver.ReadStatus();
+                    driver.CloseValve();
+                }
     }
 }
